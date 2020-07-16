bdb8298591960b8c34cf357af92ccee3a3e9e261
Riaz11-coder <riaz11@hotmail.com>
2020-07-16 18:38:27 -0400

    add CukesRunner.java

--- src/test/java/btx24/crm/runners/CukesRunner.java
+++ src/test/java/btx24/crm/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cybertek/CRM.step_definitions",
         dryRun = false,
-        tags = ""
+        tags = "@wiki"
 )
 public class CukesRunner {
 }
