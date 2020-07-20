5adc6291ad45a79778a0c86ebfc40e9995b80fef
massudatmar <massud.atmar@gmail.com>
2020-07-20 19:20:21 -0400

    Test_Massud

--- src/test/java/btx24/crm/pages/One.java
+++ src/test/java/btx24/crm/pages/One.java
@@ -1,11 +1,14 @@
 package btx24.crm.pages;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class One {
 
     public static void main(String[] args) {
 
 
         System.out.println("Hello");
+
 
     }
 }
---
+++ src/test/java/btx24/crm/pages/Test_Massud.java
@@ -0,0 +1,13 @@
+package btx24.crm.pages;
+
+public class Test_Massud {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Radda");
+
+
+    }
+}
